fdf61690bf9ec29ff4d10fdf34fa4f1684a234ec
RANGGA GALIH WARDANI <220101071@mhs.udb.ac.id>
2024-07-15 17:35:41 +0700

    first init github

---
+++ README.md
@@ -0,0 +1,16 @@
+# sistempendukungkeputusan-ahp
+Indonesia : Sistem pendukung keputusan menggunakan metode ahp, dibuat untuk memenuhi tugas perkuliahan matakuliah Sistem Pendukung Keputusan
+
+English : The decision support system uses the ahp method, made to fulfill the college assignments for the decision support system
+
+langkah menjalankan program :
+
+1. Ekstrak File Zip.
+2. Buka folder yang sudah di Ekstrak melalui software visual studio code.
+3. jalankan xampp.
+4. Import database yang sudah ada di dalam folder ke phpmyadmin.
+5. Kembali ke VsCode lalu buka file index, lalu jalankan terminal pada file index.
+6. Agar terhubung dengan localhost Jalankan perintah php -S localhost:81, melalui terminal.
+7. Buka web Browser lalu ketikkan perintah localhost:81
+8. Sistem web siap digunakan.
+ 
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SPK AHP KELOMPOK 1</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Montserrat:wght@500&family=PT+Sans&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="css/style1.css">
+</head>
+
+<body>
+  <?php include 'fungsi.php'; ?>
+  <?php include 'navbar.php'; ?>
+  <div class="container" h3 class="text-center"></h3>
+  <!DOCTYPE html>
+
+    
+   
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+          <br>
+          <br>
+            <h1>Sistem Pendukung Keputusan Menggunakan Metode AHP</h1>
+            <h5>SPK membantu pengambil keputusan dalam menyelesaikan masalah yang kompleks dan tidak terstruktur dengan memberikan informasi yang relevan, model analisis, dan alat-alat pendukung lainnya.</h5>
+            <br>
+            <br>
+        </div>
+        <div class="center">
+            <img src="image/spk.png" width="1300" height="1000">
+        </div>
+    </div>
+</body>
+
+
+    
+
+</html>
